91b9fadac80f6d00ef21fb8ed9e54c13a8cf8280
aaungdev <dhev.code@gmail.com>
2024-05-27 20:55:38 -0400

    exercise1

---
+++ assignment4/exercise1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printStudentInfo(const char names[][20], int ages[], int size) {
+    int sumAge = 0;
+    for (int i = 0; i < size; i++) {
+        cout << "Student " << i + 1 << ": " << names[i] << ", Age: " << ages[i] << endl;
+        sumAge += ages[i];
+    }
+    cout << "Sum of ages: " << sumAge << endl;
+    cout << "Average age: " << (float)sumAge / size << endl;
+}
+
+int main() {
+    const int numberOfStudents = 10;
+    const char names[numberOfStudents][20] = {
+        "John", "Emma", "Michael", "Sophia", "Daniel",
+        "Olivia", "Matthew", "Ava", "Joshua", "Isabella"
+    };
+    int ages[numberOfStudents] = {18, 19, 20, 21, 18, 19, 20, 21, 18, 19};
+
+    printStudentInfo(names, ages, numberOfStudents);
+
+    return 0;
+}
