73b68a658e0e5697c6b67c060c0d8589f15f4226
aaungdev <dhev.code@gmail.com>
2024-05-27 21:03:57 -0400

    exercise4

---
+++ assignment4/exercise4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void countdownByTwo(int number) {
+    while (number >= 0) {
+        cout << number;
+        number -= 2;
+        if (number >= 0) {
+            cout << ",";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    countdownByTwo(number);
+
+    return 0;
+}
