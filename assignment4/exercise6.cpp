352436fc0413a71f1d2bf6d707cfd22b70ba4ac2
aaungdev <dhev.code@gmail.com>
2024-05-27 21:06:32 -0400

    exercise6

---
+++ assignment4/exercise6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int number) {
+    return number % 2 == 0;
+}
+
+void checkEvenOrOdd(int number) {
+    if (isEven(number)) {
+        cout << number << " is an even number." << endl;
+    } else {
+        cout << number << " is an odd number." << endl;
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    checkEvenOrOdd(number);
+
+    return 0;
+}
