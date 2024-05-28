4f197607ef9e2770de0d126b2862b995daccb9e5
aaungdev <dhev.code@gmail.com>
2024-05-27 21:02:27 -0400

    exercise3

---
+++ assignment4/exercise3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int calculateSquare(int number) {
+    return number * number;
+}
+
+double calculateSquareRoot(int number) {
+    double guess = number / 2.0;
+    double epsilon = 0.00001;
+    while ((guess * guess - number) > epsilon || (guess * guess - number) < -epsilon) {
+        guess = (guess + number / guess) / 2.0;
+    }
+    return guess;
+}
+
+int calculateFactorial(int number) {
+    int factorial = 1;
+    for (int i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    int square = calculateSquare(number);
+    double squareRoot = calculateSquareRoot(number);
+    int factorial = calculateFactorial(number);
+
+    cout << "Square of " << number << " is: " << square << endl;
+    cout << "Square root of " << number << " is: " << squareRoot << endl;
+    cout << "Factorial of " << number << " is: " << factorial << endl;
+
+    return 0;
+}
