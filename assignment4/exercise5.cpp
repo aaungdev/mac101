e50240c1d1ec502e74a232225e64c584b88445e3
aaungdev <dhev.code@gmail.com>
2024-05-27 21:05:27 -0400

    exercise5

---
+++ assignment4/exercise5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void calculateSumAndProduct(int a, int b, int c, int &sum, int &product) {
+    sum = a + b + c;
+    product = a * b * c;
+}
+
+int main() {
+    int a, b, c;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Enter third number (or press Enter to use 2): ";
+    if (cin.peek() == '\n') {
+        c = 2;
+    } else {
+        cin >> c;
+    }
+
+    int sum, product;
+    calculateSumAndProduct(a, b, c, sum, product);
+
+    cout << "Sum: " << sum << endl;
+    cout << "Product: " << product << endl;
+
+    return 0;
+}
